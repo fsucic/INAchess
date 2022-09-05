1e94bf7a6e7891ed483b637cbc59f97e24f02da3
fsucic <fransucic46@gmail.com>
2022-09-05 20:50:57 +0200

    first commit

---
+++ src/main/java/com/example/INAchess/InAchessApplication.java
@@ -0,0 +1,13 @@
+package com.example.INAchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InAchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InAchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/INAchess/InAchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.INAchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InAchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
